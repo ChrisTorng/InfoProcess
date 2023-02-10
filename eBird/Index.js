2e1c50c6c4326fa2e42a49ba2f05915df948826a
ChrisTorng <christorng@hotmail.com>
2023-02-10 16:11:02 +0800

    update

--- eBird/Index.js
+++ eBird/Index.js
@@ -1,19 +1,20 @@
 const history = document.getElementById('history');
 const sourceElement = document.getElementById('source');
 
-quickSummary(sourceElement.value);
+//quickSummary(sourceElement.value);
 
 function quickSummary(source) {
     source = source.substring(source.indexOf('please-bird-mindfully') + 'please-bird-mindfully'.length + 2);
     source = source.substring(0, source.indexOf('***********') - 2);
+
     var records = source.split('\n\n');
-    appendHistory(records[0]);
-    appendHistory(getRecord(records[0]));
-    appendHistory(getRecord(records[1]));
-    appendHistory(getRecord(records[2]));
+    for (var record in records) {
+        appendHistory(getRecord(records[record]));
+    }
 }
 
 function getRecord(record) {
+    console.log(record);
     var lines = record.split('\n');
     var name = lines[0];
     var place = lines[2].substring(2);
@@ -29,7 +30,7 @@ function getUrlWithoutSearch(url) {
 
 function appendHistory(message) {
     message = message.replace(/(\r|\n)/g, '<br/>');
-    history.innerHTML = `${message}<div class="clear"/><br/>${history.innerHTML}`;
+    history.innerHTML = `${history.innerHTML}<div class="clear"/><br/>${message}`;
 }
 
 function getFullHtml(value) {
